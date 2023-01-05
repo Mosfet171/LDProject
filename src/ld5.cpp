a08812f64a0057a6ee1fed8c802a9aabc66b0922
Mosfet171 <arkadym@live.be>
2023-01-05 16:08:48 +0100

    Fixed ALL problems (?)

--- src/ld5.cpp
+++ src/ld5.cpp
@@ -14,10 +14,6 @@
 //#include <bits/stdc++.h>
 
 #define N 1000
-#define T_MAX 2
-#define T_MIN 0
-#define S_MAX 1
-#define S_MIN (-1)
 #define N_THREADS 8
 
 using namespace std;
@@ -30,7 +26,7 @@ int doIreplace(int px, int py, int kx, int ky, float T, float S) {
 		int Dg = maxT - minS;
 		float prob = (py-px)/(kg*Dg);
 		int random = (rand())%100;
-		if (prob > random) {
+		if (100*prob > random) {
 			return(1);
 		}
 	}
@@ -42,8 +38,13 @@ float run_execution_on_graph(short int graph[N][N], short int neighbours_sizes[]
 	random_device rd;
 	int population[N];
 	int next_population[N];
-	int payoffs[N];
-	int full_sums[n_gen];
+	float payoffs[N];
+	float full_sums[n_gen];
+
+	//cout << "P: " << payoff_matrix[0][0] << endl;
+	//cout << "T: " << payoff_matrix[0][1] << endl;
+	//cout << "S: " << payoff_matrix[1][0] << endl;
+	//cout << "R: " << payoff_matrix[1][1] << endl;
 	
 	for (int i = 0; i<N; i++) {
 		population[i] = i < N/2 ? 0 : 1;
@@ -61,7 +62,8 @@ float run_execution_on_graph(short int graph[N][N], short int neighbours_sizes[]
 		int len_neighbours; 
 		// Loop over population
 		for (int j = 0; j<N; j++) {
-			payoffs[j] = 0;
+			payoffs[i] = 0;
+
 			len_neighbours = neighbours_sizes[j];
 			for (int k = 0; k<len_neighbours; k++) {
 				payoffs[j] = payoffs[j] + payoff_matrix[population[j]][population[graph[j][k]]];
@@ -73,16 +75,16 @@ float run_execution_on_graph(short int graph[N][N], short int neighbours_sizes[]
 			len_neighbours = neighbours_sizes[j];
 			int random_neighbour_id = rand() % len_neighbours;
 			int random_neighbour_strat = graph[j][random_neighbour_id];
-			int px = payoffs[j]; int py = payoffs[random_neighbour_strat];
+			float px = payoffs[j]; float py = payoffs[random_neighbour_strat];
 			int ky = neighbours_sizes[random_neighbour_strat];
 			int kx = len_neighbours;
-			int S = payoff_matrix[1][0]; int T = payoff_matrix[0][1];
+			float S = (float)payoff_matrix[1][0]; float T = (float)payoff_matrix[0][1];
 			// DO I REPLACE REPLACEMENT:
 			if (px < py) {
-				int kg = kx > ky ? kx : ky;
-				int maxT = T > 1 ? T : 1;
-				int minS = S < 0 ? S : 0;
-				int Dg = maxT - minS;
+				float kg = kx > ky ? kx : ky;
+				float maxT = T > 1 ? T : 1;
+				float minS = S < 0 ? S : 0;
+				float Dg = maxT - minS;
 				float prob = ((float)py-(float)px)/((float)kg*(float)Dg)*100;
 				int random = (rand())%100;
 				if (prob > random) {
@@ -116,7 +118,7 @@ float run_execution_on_graph(short int graph[N][N], short int neighbours_sizes[]
 }
 
 void retrieve_graph(int i, short int graph[N][N], short int neighbours_sizes[]) {
-	string fname = "py_graph_"+to_string(i)+".txt";
+	string fname = "/Users/mos/Documents/ULB/MA3.tmp/INFO-F409/Exam/LDProject/graphs/py_graph_"+to_string(i)+".txt";
 	fstream thefile (fname, ios::in);
 
 	vector<vector<string> > content;
@@ -150,6 +152,7 @@ void retrieve_graph(int i, short int graph[N][N], short int neighbours_sizes[]) 
 
 float exec_with_fixed_params(float P, float T, float S, float R, int n_real, int n_runs, int n_gen, int n_transient) {
 	float payoff_matrix[2][2] = {{P,T},{S,R}};
+
 	float sums_real_av = 0;
 	float sums_av = 0;
 
@@ -171,36 +174,39 @@ float exec_with_fixed_params(float P, float T, float S, float R, int n_real, int
 	return sums_real_av; 
 }
 
-void exec_on_thread(int tid) {
-	int P = 0; int R = 1; float T = 1; float S = -1; 
-	int n_real = 10; int n_runs = 10;
-	int n_transient = 1000;
-	int n_gen = 1000; 
-	int granularity = 8;
 
-	/*
-	int* pptid = (int*) p_tid;
-	int tid = *pptid;
-	*/
 
-	// Loop Over S's
+//void exec_on_thread(int tid) {
+//	int P = 0; int R = 1; float T = 1; float S = -1; 
+//	int n_real = 10; int n_runs = 10;
+//	int n_transient = 1000;
+//	int n_gen = 1000; 
+//	int granularity = 8;
+//
+//	/*
+//	int* pptid = (int*) p_tid;
+//	int tid = *pptid;
+//	*/
+//
+//	// Loop Over S's
+//
+//	for (int i=tid*(granularity/N_THREADS);i<(tid+1)*(granularity/N_THREADS);i++) {
+//		float S = ((float)S_MAX-(float)S_MIN)/(float)granularity * (float)i + (float)S_MIN;
+//		cout << "[Thread " << tid << "] ---------- S = " << S << " ---------- #" << endl;
+//
+//		string outname = "out_S_" + to_string(i) + ".txt";
+//		fstream outfile (outname, ios::app);
+//		// Loop over T's
+//		for (int j=0;j<granularity;j++) {
+//			float T = ((float)T_MAX-(float)T_MIN)/(float)granularity * (float)i + (float)T_MIN;
+//			cout << "[Thread " << tid << "] ----- T = " << T << " ----- #" << endl;
+//			float thesum = exec_with_fixed_params(P,T,S,R,n_real,n_runs,n_gen,n_transient);
+//			cout << "[Thread " << tid << "] Sum: " << thesum << endl;
+//			outfile << thesum << endl;
+//		}
+//	}	
+//}
 
-	for (int i=tid*(granularity/N_THREADS);i<(tid+1)*(granularity/N_THREADS);i++) {
-		float S = (S_MAX-S_MIN)/(float)granularity * (float)i + (float)S_MIN;
-		cout << "[Thread " << tid << "] ---------- S = " << S << " ---------- #" << endl;
-
-		string outname = "out_S_" + to_string(i) + ".txt";
-		fstream outfile (outname, ios::app);
-		// Loop over T's
-		for (int j=0;j<granularity;j++) {
-			float T = (T_MAX-T_MIN)/(float)granularity * (float)i + (float)T_MIN;
-			cout << "[Thread " << tid << "] ----- T = " << T << " ----- #" << endl;
-			float thesum = exec_with_fixed_params(P,T,S,R,n_real,n_runs,n_gen,n_transient);
-			cout << "[Thread " << tid << "] Sum: " << thesum << endl;
-			outfile << thesum << endl;
-		}
-	}	
-}
 
 void progressBar(float progress) {
     int barWidth = 48;
@@ -228,6 +234,12 @@ int main(int argc, char** argv) {
 	int n_transient = stoi(argv[1]);
 	int n_gen = stoi(argv[2]);
 	int granularity = 64;
+
+	float T_MAX = 2;
+	float T_MIN = 0;
+
+	float S_MAX = 1;
+	float S_MIN = -1;
 
 	int tid = 5;
 
